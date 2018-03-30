3b8ec64d9f5b9db200df2d9c5e4b66d413cbb7b1
Aleksandra Lazoroska <aleksandra.lazoroska@gmail.com>
2018-03-30 13:31:39 +0200

    Marketing promote page and the observer after placing order

--- app/code/community/Javen/CustomerAsAProduct/Model/Observer.php
+++ app/code/community/Javen/CustomerAsAProduct/Model/Observer.php
@@ -22,4 +22,34 @@ class Javen_CustomerAsAProduct_Model_Observer {
 
 			}
 
+			public function marketingPromotedProduct(Varien_Event_Observer $observer) {
+
+				$event = $observer->getEvent();
+        $order = $event->getOrder();
+
+				$productSku = '';
+
+        foreach ($order->getAllVisibleItems() as $item) {
+						$productSku = $item->getProduct()->getSku();
+        }
+
+				if (strpos($productSku, 'marketing-package-main-product-package') !== false ) {
+
+					$marketingProductId = substr($productSku, strrpos( $productSku, '-' ) + 1);
+
+					// Load customer by ID
+					$customer = Mage::getModel('customer/customer')->load($order->getCustomerId());
+					$businessPage = $customer->getBusinessPage();
+
+					// Load Product by Business ID
+					$product = Mage::getModel('catalog/product')->load($businessPage);
+					$product->setMarketingPromoted($marketingProductId);
+					$product->save();
+
+				}
+
+				//Mage::log($quote, null, 'development.log');
+
+			}
+
 }
